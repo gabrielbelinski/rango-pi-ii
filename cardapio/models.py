221a8527a942dfb1a386e6487b90460b9f55a006
Gabriel Belinski <gabrielbelinskisalomao@gmail.com>
2023-11-13 10:18:57 -0300

    models modificadas

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=50, null=False, blank=False)
+    preco = models.FloatField(null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    imagem = models.CharField(max_length=100, null=False, blank=False)
+    disponibilidade = models.BooleanField(default=True)
+
+    def __str__(self):
+            return f"Produto [nome={self.nome}]"
+
--- cardapio/views.py
+++ cardapio/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from cardapio.models import Produto
+
 # Create your views here.
+
+produtos = {}
 def index(request):
     return render(request, 'cardapio/index.html')
+
+def exibeProdutos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'cardapio/index.html', {'produtos':produtos})
+
+
